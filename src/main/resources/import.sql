27703996c3d3c3f9ba8de47cab66ae81de44f711
KalyanBY12 <kalyan.chowdhury@jda.com>
2022-10-03 08:59:29 +0530

    Initial commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(1, 'Kalyan','Chowdhury','kc0@gmail.com','123456', null);
+INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(2, 'Kalyan2','Chowdhury2','kc201@gmail.com','asdfgh', null);
+INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(3, 'Maya','Roy','mc282@gmail.com','qwerty', null);
+INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(4, 'Abhi','Kumar','ak0@gmail.com','anivu', null);
+INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(5, 'John','Singh','jk0@gmail.com','anivu', null);
+INSERT INTO Learners(learner_id, learner_first_name, learner_last_name, learner_email, learner_password, course_id) VALUES(6, 'rohit','roy','rr0@gmail.com','anivu', null);
